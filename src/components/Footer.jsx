0bc8e3bc3bdaf78e60cc529c8e28b3f6435ec108
Michael <m_reinert@outlook.com>
2023-09-26 13:36:32 +0200

    deleted unnecessary links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,6 @@
 import React from 'react';
 import { Typography } from '@material-tailwind/react';
 
-const LINKS = [
-  {
-    title: "Product",
-    items: ["Overview", "Features", "Solutions", "Tutorials"]
-  },
-  {
-    title: "Company",
-    items: ["About us", "Careers", "Press", "News"]
-  },
-  {
-    title: "Resource",
-    items: ["Blog", "Newsletter", "Events", "Help center"]
-  }
-]
-
 const currentYear = new Date().getFullYear();
 
 export default function Footer() {
