065853565047bb21a24d3713903760272cee8518
Michael <m_reinert@outlook.com>
2023-09-27 16:54:19 +0200

    optimized homepage for mobile phone

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -4,10 +4,10 @@ export default function Hero() {
   return (
     <div className='flex flex-col lg:flex-row h-250 md:h-300'>
       <div className='basis-3/5 bg-black text-white flex flex-col'>
-        <div className='mt-32 w-3/4'>
-          <h2 className='text-red-700 w-1/2 text-7xl md:mr-auto mx-auto md:ml-32 mb-8 font-semibold'>Kohlrabi</h2>
-          <h3 className='text-4xl md:text-5xl mb-12 md:mb-20 w-3/4 text-left md:mr-auto ml-24 md:ml-32 font-semibold'>Wir haben alles was Du benötigst</h3>
-          <button className='bg-gray-700 w-2/5 mb-12 md:py-3 px-28 md:px-0 ml-20 md:ml-auto md:mr-20 font-semibold'>Filialen entdecken</button>
+        <div className='mt-32 w-4/6 mx-auto md:mx-0'>
+          <h2 className='text-red-700 md:w-1/2 text-7xl md:mr-auto md:ml-32 mb-8 font-semibold'>Kohlrabi</h2>
+          <h3 className='text-4xl md:text-5xl mb-12 md:mb-20 md:w-3/4 md:text-left md:mr-auto md:ml-32 font-semibold'>Wir haben alles was Du benötigst</h3>
+          <button className='bg-gray-700 w-2/5 mb-12 md:py-3 px-28 md:px-0 md:ml-auto md:mr-20 font-semibold'>Filialen entdecken</button>
         </div>
       </div>
       <div className='invisible lg:visible md:bg-black md:basis-3/5 md:container'>
--- src/components/Home/Questions.jsx
+++ src/components/Home/Questions.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Questions() {
   return (
-    <div className='flex flex-col mt-20 items-center mb-20'>
-      <h3 className='text-6xl font-semibold'>Gibt es weitere Fragen?</h3>
-      <button className='bg-red-700 text-white w-3/12 py-4 text-xl font-semibold mt-16'>Kontaktiere uns</button>
+    <div className='flex flex-col mt-12 md:mt-20 items-center mb-20'>
+      <h3 className='text-4xl md:text-6xl font-semibold'>Gibt es weitere Fragen?</h3>
+      <button className='bg-red-700 text-white md:w-3/12 w-2/4 py-4 text-xl font-semibold mt-12 md:mt-16'>Kontaktiere uns</button>
     </div>
   )
 }
