0992e5f57ad6844cf1a36d17d616d5fbc525be68
Michael <m_reinert@outlook.com>
2023-10-13 19:20:33 +0200

    included swiper with slideshow on homepage

--- src/components/Home/Filialen.jsx
+++ src/components/Home/Filialen.jsx
@@ -8,14 +8,29 @@ import locations from '../../data/locations';
 
 export default function Filialen() {
   return (
-    <div className='w-3/4 mx-auto'>
+    <div className='w-3/4 xl:w-9/12 mt-12 mx-auto text-left'>
+      <h2 className='text-5xl font-bold mb-4'>Unsere Standorte</h2>
+      <h3 className='text-xl mb-4'>Geniesse unsere köstlichen Gerichte an 6 Standorten in Deutschland</h3>
+      <p className='mb-10'>Jedes auf seine Art irgendwie anders – doch immer das Original bona’me mit großer Liebe zum Detail und den authentischen Speisen. Genieße Deine Pide gleich 3 x in Köln, die leckeren Makarna am Mittag in Hamburg, das legendäre Nan û Köfte in Hannover die Meze in Dortmund oder alles zusammen im neuen bona’me Münster.</p>
+
+
       <Swiper
         // install Swiper modules
+        className='w-full'
         modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
         spaceBetween={50}
         autoplay={{ delay: 2500, disableOnInteraction: false }}
         onAutoplayStart={2000}
-        slidesPerView={3}
+        breakpoints={{
+          1024: {
+            width: 1024,
+            slidesPerView: 1
+          },
+          1367: {
+            width: 1367,
+            slidesPerView: 3
+          }
+        }}
         navigation
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
